9c01c8df0aab9ece4de8b2ef930a938c4380018b
Guriy Samarin <guriysamarin@outlook.com>
2019-08-14 22:16:06 +0300

    ItemTemplate added

---
+++ In2TeamsSplitter/In2TeamsSplitter/NameAndRating.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace In2TeamsSplitter
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NameAndRating : Grid
+    {
+        public NameAndRating()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty NameProperty = BindableProperty.Create(nameof(Name), typeof(string), typeof(NameAndRating), defaultBindingMode: BindingMode.TwoWay);
+
+        public string Name
+        {
+            get => (string)GetValue(NameProperty);
+            set => SetValue(NameProperty, value);
+        }
+    }
+}
